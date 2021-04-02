2ff733c35460294d131a3e577d9bd94f6670e36a
curder <q.curder@gmail.com>
2021-04-02 10:02:51 +0800

    Add smsController test

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/sms', 'SmsController@sendSmsCode')->name('sms')->middleware(['sms_access_token']); // 发送短信验证码
---
+++ tests/Unit/Integration/Http/Controllers/SmsControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Tests\Unit\Integration\Http\Controllers;
+
+use App\Http\Controllers\SmsController;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SmsControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    // 发送验证码
+    public function it_can_send_sms() : void
+    {
+        $response = $this->postJson('/api/sms', ['phone' => 13800138000]);
+        $this->assertDatabaseHas('laravel_sms', [
+            'to' => 13800138000
+        ]);
+        $response->assertOk()->assertJson([
+                'success' => true, 'type' => 'sms_sent_success',
+                'message' => config('laravel-sms.notifies.sms_sent_success')
+            ]);
+    }
+}
